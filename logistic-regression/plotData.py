225ebef5c354c8679af1245e41032bd06e976e70
quartox <jessewlord@gmail.com>
2015-01-09 08:10:41 -0800

    Changed the plot function to show different signs for positive or negative signals, also added best fit line of logistic regression.

--- logistic-regression/plotData.py
+++ logistic-regression/plotData.py
@@ -4,7 +4,21 @@ __author__="Jesse Lord"
 __date__="January 8, 2015"
 
 import matplotlib.pyplot as p
+import numpy as np
 
-def plotData(x,y):
-    p.scatter(x[0,:],x[1,:],c=y)
+def plotPoints(x,y):
+    pos = np.where(y==1)
+    neg = np.where(y==0)
+    p.scatter(x[0,pos],x[1,pos],marker='o')
+    p.scatter(x[0,neg],x[1,neg],marker='x')
+    p.show()
+
+def plotTheta(x,y,theta):
+    pos = np.where(y==1)
+    neg = np.where(y==0)
+    p.scatter(x[0,pos],x[1,pos],marker='o')
+    p.scatter(x[0,neg],x[1,neg],marker='x')
+    plotx = np.array([np.amin(x[0,:]),np.amax(x[0,:])])
+    ploty = np.array([np.amin(x[1,:]),np.amax(x[1,:])])
+    p.plot(ploty,-(theta[0]+ploty*theta[2])/theta[1])
     p.show()
