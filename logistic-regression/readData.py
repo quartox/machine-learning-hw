282d4b4f5c00bf3f359bc365c99bb77ab860f7ff
quartox <jessewlord@gmail.com>
2015-01-09 09:08:34 -0800

    Read second data set.

--- logistic-regression/readData.py
+++ logistic-regression/readData.py
@@ -11,21 +11,39 @@ nfeatures = 2
 def readFirst():
     x = np.empty([2,1])
     temp = np.empty([2,1])
-    y = np.empty(1)
+    y = np.empty(1,dtype=float)
     with open('ex2data1.txt','r') as f:
         datafile = csv.reader(f)
-        ii = 0
+        ii = 1
         for line in datafile:
-            if ii==0:
+            if ii==1:
                 x[0,0] = line[0]
                 x[1,0] = line[1]
-                y[0] = line[2]
+                y[0] = np.float_(line[2])
             temp[:,0] = line[:2]
             x=np.append(x,temp,axis=1)
-            y=np.append(y,line[2])
+            y=np.append(y,np.float_(line[2]))
             ii += 1
         # endfor line in datafile:
     # close file
-    print x[0,0],x[1,0],y[0],x[0,-1],x[1,-1]
-    print x.shape,y.shape
+    return (x,y,ii)
+
+def readSecond():
+    x = np.empty([2,1])
+    temp = np.empty([2,1])
+    y = np.empty(1,dtype=float)
+    with open('ex2data2.txt','r') as f:
+        datafile = csv.reader(f)
+        ii = 1
+        for line in datafile:
+            if ii==1:
+                x[0,0] = line[0]
+                x[1,0] = line[1]
+                y[0] = np.float_(line[2])
+            temp[:,0] = line[:2]
+            x=np.append(x,temp,axis=1)
+            y=np.append(y,np.float_(line[2]))
+            ii += 1
+        # endfor line in datafile:
+    # close file
     return (x,y,ii)
