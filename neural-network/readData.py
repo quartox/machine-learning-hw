99a6b985334e569a3c7ca91e17e5c86e0d418a17
quartox <jessewlord@gmail.com>
2015-01-12 10:25:08 -0800

    Read the matlab data file and neural network pre-determined wweights.

---
+++ neural-network/readData.py
@@ -0,0 +1,18 @@
+"""Reads in the data from the matlab files."""
+
+__author__="Jesse Lord"
+__date__="January 12, 2015"
+import scipy.io as io
+
+def readData():
+    mat = io.loadmat("ex3data1.mat")
+    y = mat['y']
+    X = mat['X']
+    return (X,y)
+
+def readWeights():
+    mat = io.loadmat("ex3weights.mat")
+    print mat.keys()
+    Theta1 = mat['Theta1']
+    Theta2 = mat['Theta2']
+    return (Theta1,Theta2)
