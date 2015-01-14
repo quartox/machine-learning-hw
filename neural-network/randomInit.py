a39bb0908af8cd8c2f11f96a935db81b83f24a61
quartox <jessewlord@gmail.com>
2015-01-14 08:49:52 -0800

    Randomizes the initial weights in the range of epsilon.

--- neural-network/randomInit.py
+++ neural-network/randomInit.py
@@ -6,6 +6,7 @@ __date__="January 13, 2015"
 import numpy as np
 
 def randomInit(L_in,L_out):
+    """Randomizes weights in range [-epsilon,epsilon). We choose epsilon=sqrt(6)/sqrt(L_in+L_out) where L_in is the number of input connections and L_out is the number of output connections in the neural network."""
     epsilon = np.sqrt(6.0) / np.sqrt( L_in + L_out )
     weights = np.random.rand(L_out,L_in+1)
     weights -= 0.5 # centers random numbers on 0.0
