87a9cbd0d24c52f3e7092d1c7da5ac6d6f15ca06
quartox <jessewlord@gmail.com>
2015-01-15 09:30:33 -0800

    Added optional x-axis input to plotLearning and changed name to plotError.
    This allows the same function to plot the validation error for the regularization parameter (or other validations).

--- learning-curve/plotData.py
+++ learning-curve/plotData.py
@@ -5,7 +5,6 @@ __date__="January 8, 2015"
 
 import matplotlib.pyplot as p
 import numpy as np
-from mapFeature import mapFeature
 from polyFeatures import polyFeatures
 
 def plotPoints(x,y):
@@ -49,38 +48,12 @@ def plotNorm(X,y,theta,mu,sigma):
     p.plot(xplot,yplot,'r')
     p.show()
 
-def plotLearning(error_train,error_cv):
-    n = len(error_train)
-    x = np.arange(n)+1
+def plotError(error_train,error_cv,x=None):
+    """Plots the training error and cross validation error for a given range of parameters. If no x-axis is sent to function then it is assumed to be learning curve with number of training examples as x-axis."""
+    if x is None:
+        n = len(error_train)
+        x = np.arange(n)+1
     p.plot(x,error_train,'g')
     p.plot(x,error_cv,'b')
 
-    p.show()
-
-def plotReg(x,y,theta,degree):
-    pos = np.where(y==1)
-    neg = np.where(y==0)
-    p.scatter(x[0,pos],x[1,pos],marker='o')
-    p.scatter(x[0,neg],x[1,neg],marker='x')
-    nx = 100
-    rangex = np.array([np.amin(x[0,:]),np.amax(x[0,:])])
-    dx = (rangex[1]-rangex[0])/nx
-    ny = 100
-    rangey = np.array([np.amin(x[1,:]),np.amax(x[1,:])])
-    dy = (rangey[1]-rangey[0])/ny
-    xplot = np.empty([2,1])
-    xcontour = np.empty(nx)
-    ycontour = np.empty(ny)
-    zcontour = np.empty([nx,ny])
-    for ii in range(nx):
-        for jj in range(ny):
-            xplot[0,0] = rangex[0] + ii*dx
-            xcontour[ii] = xplot[0,0]
-            xplot[1,0] = rangey[0] + jj*dy
-            ycontour[jj] = xplot[1,0]
-            (zplot,nfeatures) = mapFeature(xplot,degree)
-            zcontour[ii,jj] = np.dot(zplot[:,0],theta)
-        # endfor jj in range(ny)
-    # endfor ii in range(nx)
-    p.contour(xcontour,ycontour,zcontour,levels=[0])
     p.show()
